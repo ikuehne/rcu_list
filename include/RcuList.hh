5a22c7693c52bc0f575aad7336ed4b85cf10d8c8
Ian Kuehne <ikuehne617@gmail.com>
2019-09-21 16:33:00 -0700

    Comment up the RCU list.

--- include/RcuList.hh
+++ include/RcuList.hh
@@ -25,13 +25,29 @@ public:
         bool success;
 
         do {
+            // Use RCU for ABA protection.
             manager.readLock();
+            // This load synchronizes-with committing CAS-es, so that we
+            // always read the updated next pointer.
             oldHead = head.load(std::memory_order_acquire);
             if (oldHead == nullptr) break;
             RcuListNode *newHead = oldHead->next.load(
-                std::memory_order_acquire);
+                std::memory_order_relaxed);
+            // Synchronizes-with reading next pointers, so that they always
+            // get the new value.
+            //
+            // This CAS is not subject to the ABA problem due to RCU
+            // protection. If the head at this CAS is equal to oldHead, either:
+            //
+            //   1. It has not been modified since we read it. This is fine.
+            //   2. Someone popped it off the stack, and then it eventually
+            //      got pushed back on.
+            //
+            // We always delete nodes after popping them, so in case (2) we
+            // must at some point have deleted oldHead. But this explicitly
+            // violates our RCU guarantees, so case (2) is impossible.
             success = head.compare_exchange_weak(oldHead, newHead, 
-                std::memory_order_acq_rel);
+                std::memory_order_release);
             manager.readUnlock();
         } while (!success);
 
@@ -54,11 +70,13 @@ public:
 
         bool success;
         do {
+            // See the comments in pop for an explanation of how
+            // this is synchronized.
             manager.readLock();
             RcuListNode *old = head.load(std::memory_order_acquire);
-            newNode->next = old;
+            newNode->next.store(old, std::memory_order_relaxed);
             success = head.compare_exchange_weak(old, newNode,
-                std::memory_order_acq_rel);
+                std::memory_order_release);
             manager.readUnlock();
         } while (!success);
     }
@@ -66,9 +84,9 @@ public:
     bool search(RcuManager &manager, std::uint64_t data) {
         manager.readLock();
 
-        for (RcuListNode *cur = head.load(std::memory_order_relaxed);
+        for (RcuListNode *cur = head.load(std::memory_order_acquire);
              cur != nullptr;
-             cur = cur->next.load(std::memory_order_relaxed)) {
+             cur = cur->next.load(std::memory_order_acquire)) {
             if (cur->data == data) {
                 manager.readUnlock();
                 return true;
